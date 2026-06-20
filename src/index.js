894273f709ca2a872c687efe160bb03acd2b92df
Anvar Obidov <varan193@mail.ru>
2026-06-20 22:56:26 +0300

    Подготовиетльный урок

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+
+import "./style/style.scss";
+
+const marvelService = new MarvelService();
+console.log('ghbsdf');
+marvelService
+	.getAllCharacters()
+	.then((res) => res.data.results.forEach((item) => console.log(item.name)));
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
