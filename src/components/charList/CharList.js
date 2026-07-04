391b25fed1db0b522d585a0bc8a2753bf6dc89c8
Anvar Obidov <varan193@mail.ru>
2026-07-04 20:27:19 +0300

    Урок 29

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import {Component} from 'react';
+import PropTypes from 'prop-types'
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -108,6 +109,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propType = {
+    onCharSelected: PropTypes.func
 }
 
 export default CharList;
